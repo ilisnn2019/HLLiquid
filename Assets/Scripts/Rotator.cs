875ddab372cbee7254b70dc0271efdf7e4d1b74f
ILISNN <ilisnn2019@gmail.com>
2024-04-04 07:57:29 +0900

    add liquid

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -7,6 +7,11 @@ public class Rotator : MonoBehaviour{
     public float speed;
 
     void Update(){
-        transform.eulerAngles += Vector3.up * speed;
+        transform.eulerAngles += Vector3.right * speed;
+
+        if(transform.rotation.x == 90)
+        {
+            transform.eulerAngles = new Vector3(-180, 0, 0);
+        }
     }
 }
